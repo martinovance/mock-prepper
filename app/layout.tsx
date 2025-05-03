49c4a73ee29af7527936e4604f7a54be9df7c590
Lance Martin <martinsogunsina@gmail.com>
2025-05-03 16:47:55 +0100

    Initialization and layout configuration

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Mock Prepper",
+  description: "An AI powered mock interview prepapration platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.variable} antialiased pattern`}>
+        {children}
+      </body>
+    </html>
+  );
+}
