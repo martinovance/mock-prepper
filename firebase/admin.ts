c94deba04bae1e1a01d2eced593011b80f22092b
Lance Martin <martinsogunsina@gmail.com>
2025-05-05 12:07:20 +0100

    Firbase auth and db setup

---
+++ firebase/admin.ts
@@ -0,0 +1,24 @@
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const initFirebaseAdmin = () => {
+  const apps = getApps();
+
+  if (!apps.length) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIRBASE_PROJECT_PROJECT_ID,
+        clientEmail: process.env.FIRBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIRBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
+    });
+  }
+
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
+};
+
+export const { auth, db } = initFirebaseAdmin();
---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlgDT_s_l9mMrCY4ApqJz0bVFs50Jkpo4",
+  authDomain: "mockprepper.firebaseapp.com",
+  projectId: "mockprepper",
+  storageBucket: "mockprepper.firebasestorage.app",
+  messagingSenderId: "232793467732",
+  appId: "1:232793467732:web:e083bd44381713d7c876c1",
+  measurementId: "G-BJVLL59YP5",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
