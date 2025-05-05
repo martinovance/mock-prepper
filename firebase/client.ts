de19276b8032c53222852f52dcb54675b238e83b
Lance Martin <martinsogunsina@gmail.com>
2025-05-05 12:20:39 +0100

    updated env file

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "mockprepper.firebaseapp.com",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: "mockprepper.firebasestorage.app",
-  messagingSenderId: "232793467732",
-  appId: "1:232793467732:web:e083bd44381713d7c876c1",
-  measurementId: "G-BJVLL59YP5",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
